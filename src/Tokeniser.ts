51796029efc24b0f8f590c8d0020bdd08591de77
John Costa <johncosta027@gmail.com>
2022-08-04 14:21:35 +0100

    initial commit

--- src/Tokeniser.ts
+++ src/Tokeniser.ts
@@ -1,7 +1,6 @@
-export type Punc = "(" | ')' | '{' | '}';
+export type Punc = '(' | ')' | '{' | '}';
 export type Num = number;
 export type Str = string;
-export type Keyword = "if" | "then" | "lambda";
+export type Keyword = 'if' | 'then' | 'lambda';
 export type Var = string;
-export type Op = "!=" | "==" | "=";
-
+export type Op = '!=' | '==' | '=';
